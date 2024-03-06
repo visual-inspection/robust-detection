8ef0189f6af62da63dce0088d970e01a39516c30
Sebastian Hönel <sebastian.honel@lnu.se>
2024-03-06 08:14:57 +0100

    Refactor BnQn app and ascertain it's in eval mode

--- apps/extract_features/BatchNormQuantNorm.py
+++ apps/extract_features/BatchNormQuantNorm.py
@@ -23,14 +23,18 @@ from numpy import load, save
 if __name__ == '__main__':
     dev = 'cuda' if cuda.is_available() else 'cpu'
 
-    file = DATA_FOLDER.joinpath('./mvtec_cable_eq_features-scoring/train-good-CNxV2_DepthAE.npy')
-    temp = tensor(data=load(file=file)).to(device=dev)
-    b = BnQn1d(input_shape=temp.shape[1:], dev=dev, load_data=file)
-    torch.save(obj=b.state_dict(), f=MODELS_FOLDER.joinpath(f'./{BnQn1d.__name__}_DepthAE_ConvNeXt_V2.torch'))
+    bnQn: BnQn1d = None
 
     for stem in ['train-good', 'test-good', 'test-defect']:
-        # Let's inference all the files with the same model as trained on good data.
         file = DATA_FOLDER.joinpath(f'./mvtec_cable_eq_features-scoring/{stem}-CNxV2_DepthAE.npy')
         temp = tensor(data=load(file=file)).to(device=dev)
-        res = b.forward(temp).detach().cpu().numpy()
+
+        if stem == 'train-good':
+            # Train the model with this data.
+            bnQn = BnQn1d(input_shape=temp.shape[1:], dev=dev, load_data=file)
+            bnQn.eval()
+            torch.save(obj=bnQn.state_dict(), f=MODELS_FOLDER.joinpath(f'./{BnQn1d.__name__}_DepthAE_ConvNeXt_V2.torch'))
+        
+        # Let's inference all the files with the same model as trained on good data (even train-good).
+        res = bnQn.forward(temp).detach().cpu().numpy()
         save(file=DATA_FOLDER.joinpath(f'./mvtec_cable_eq_features-scoring/{stem}-CNxV2_DepthAE_BnQn1d.npy'), arr=res)
